c582fbdd67f675bb847d94fb5c30d3abca98aa93
felipereco2003-hue <felipereco2003@gmail.com>
2025-11-23 16:41:09 -0500

    Add files via upload

---
+++ mi_biografia/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Biografía</title>
+    <link rel ="stylesheet" href= "CSS/decoracion.css">
+</head>
+<body>
+
+<header>  
+    <h1>BIOGRAFÍA</h1>
+</header> 
+
+<section class="perfil"> 
+   <img src="imagen/foto.jpg"  alt="foto" class="foto-perfil">
+
+   <div class="info-personal"> 
+    <h2 class ="nombre">ADRIAN FELIPE REGALADO COLLAGUAZO</h2>
+     
+    <p><strong>Lugar de nacimiento:</strong>Loja</p>
+     <p><strong>Fecha de nacimiento:</strong>23 de febrero de 2003</p>
+    
+     </div>
+     </section>
+
+     <section class="Presentación">
+     <h2>Presentación personal</h2>
+
+
+     <div class="cuadro">  
+     <p class ="texto2">Desde pequeño he sido una persona curiosa y dedicado al aprendizaje, creci en un entorno familiar que siempre me ha brindado apoyoy motivación para avnazar, tengo 22 años y soy una persona con discapacidad física que lucha por salir adelante cada dia de mi vida. </p>
+    
+     <p>Inicie mis estudios en la Escuela particular San francisco Javier, donde adquiri bases académicas y valores fundamentales, continue mi formación en el colegio particular Isidro Ayora Cueva, etapa en la que fortaleci mi interes por la tecnología, actualmente curso la carrera de la Tecnología de la Información en la UTPL donde continuo desarrollando mis habilidades en el ámbito tecnológico.</p> 
+    
+     <P>Mi trayectoria se ha construido con disciplina y un constante deseo de superación orientado a crecer personal y profesionalmente.</p>
+
+     </div>
+     </section>
+
+     <section class="hobbies">
+     
+     <?php echo "<h2>Hobbies</h2>";
+     ?>
+     
+     <ul>
+        <li>Terapía física</li>
+        <li>Natación</li>
+     </ul>
+     </section>
+
+     <section class="contactos">
+        <h2>Contacto</h2>
+        <a  href="formulario.html">formulario</a>
+
+
+    
+    </section>
+    <footer>
+       <p>Biografía de Adrian</p> 
+    </footer>
+      
+</body>
+</html>
